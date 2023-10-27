b7e009b69ea7d0e2e8873c917e87be5da2c8585d
dmmontoto <dmontoto@ucsd.edu>
2023-10-26 22:53:15 -0700

    added user controller

---
+++ controllers/userController.js
@@ -0,0 +1,61 @@
+const { User, Thought } = require('../../models');
+
+module.exports = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find().populate('thoughts friends');
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findById(req.params.id).populate('thoughts friends');
+      if (!user) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!updatedUser) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const deletedUser = await User.findByIdAndRemove(req.params.id);
+      if (!deletedUser) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+      await Thought.deleteMany({ _id: { $in: deletedUser.thoughts } });
+      res.status(204).end();
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
