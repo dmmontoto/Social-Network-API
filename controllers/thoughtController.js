f9f3a1cba3cc5b13defd77d538d852330a025044
dmmontoto <dmontoto@ucsd.edu>
2023-10-26 22:55:50 -0700

    added thought/reaction routing

---
+++ controllers/thoughtController.js
@@ -0,0 +1,71 @@
+const { Thought } = require('../../models');
+
+module.exports = {
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.id);
+      if (!thought) {
+        res.status(404).json({ message: 'Thought not found' });
+        return;
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createThought(req, res) {
+    try {
+      const newThought = await Thought.create(req.body);
+      res.status(201).json(newThought);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+
+  async createReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $push: { reactions: req.body } },
+        { new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+
+      res.status(201).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Route to remove a reaction from a thought's reactions array field
+  async removeReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+
+      res.status(200).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
